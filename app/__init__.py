d25df5174fe0d1e393d62944f0c5058e9fd9c21a
glasses-png <cchelangat16@gmail.com>
2020-05-07 17:52:39 +0300

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_mail import Mail
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_uploads import IMAGES, UploadSet,configure_uploads
+from .flask_uploads import IMAGES, UploadSet,configure_uploads
 
 db = SQLAlchemy()
 mail = Mail()
